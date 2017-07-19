e043e6a504c05e584ae2eea18f39c51259d13681
masaiio <Kellielazo@gmail.com>
2017-07-19 12:23:01 -0400

    Removed all joke generators besides rand joke

--- src/Leto2bot/Modules/Searches/Commands/JokeCommands.cs
+++ src/Leto2bot/Modules/Searches/Commands/JokeCommands.cs
@@ -24,16 +24,6 @@ namespace Leto2bot.Modules.Searches
             }
 
             [Leto2Command, Usage, Description, Aliases]
-            public async Task Yomama()
-            {
-                using (var http = new HttpClient())
-                {
-                    var response = await http.GetStringAsync("http://api.yomomma.info/").ConfigureAwait(false);
-                    await Context.Channel.SendConfirmAsync(JObject.Parse(response)["joke"].ToString() + " 😆").ConfigureAwait(false);
-                }
-            }
-
-            [Leto2Command, Usage, Description, Aliases]
             public async Task Randjoke()
             {
                 using (var http = new HttpClient())
@@ -50,41 +40,6 @@ namespace Leto2bot.Modules.Searches
 
                     await Context.Channel.SendConfirmAsync("", part1 + "\n\n" + part2, footer: document.BaseUri).ConfigureAwait(false);
                 }
-            }
-
-            [Leto2Command, Usage, Description, Aliases]
-            public async Task ChuckNorris()
-            {
-                using (var http = new HttpClient())
-                {
-                    var response = await http.GetStringAsync("http://api.icndb.com/jokes/random/").ConfigureAwait(false);
-                    await Context.Channel.SendConfirmAsync(JObject.Parse(response)["value"]["joke"].ToString() + " 😆").ConfigureAwait(false);
-                }
-            }
-
-            [Leto2Command, Usage, Description, Aliases]
-            public async Task WowJoke()
-            {
-                if (!_searches.WowJokes.Any())
-                {
-                    await ReplyErrorLocalized("jokes_not_loaded").ConfigureAwait(false);
-                    return;
-                }
-                var joke = _searches.WowJokes[new Leto2Random().Next(0, _searches.WowJokes.Count)];
-                await Context.Channel.SendConfirmAsync(joke.Question, joke.Answer).ConfigureAwait(false);
-            }
-
-            [Leto2Command, Usage, Description, Aliases]
-            public async Task MagicItem()
-            {
-                if (!_searches.WowJokes.Any())
-                {
-                    await ReplyErrorLocalized("magicitems_not_loaded").ConfigureAwait(false);
-                    return;
-                }
-                var item = _searches.MagicItems[new Leto2Random().Next(0, _searches.MagicItems.Count)];
-
-                await Context.Channel.SendConfirmAsync("✨" + item.Name, item.Description).ConfigureAwait(false);
             }
         }
     }
